37a3dda471e7d009f53c1902a42054905c70e0ef
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-03-09 17:10:14 +0100

    logout button

--- mobile/src/main/java/myhealthylife/androidapp/myhealthylifemobile/MainActivity.java
+++ mobile/src/main/java/myhealthylife/androidapp/myhealthylifemobile/MainActivity.java
@@ -6,11 +6,24 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String USERNAME_PREF_NAME="USERNAME_PREF_NAME";
     private String username=null;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu,menu);
+
+        return true;
+    }
 
     @Override
     protected void onResume() {
@@ -28,6 +41,24 @@ public class MainActivity extends AppCompatActivity {
             Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logout:
+                /*remove login informations*/
+                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key),Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove(USERNAME_PREF_NAME).apply();
+
+                /*reload the activity*/
+                finish();
+                startActivity(getIntent());
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
